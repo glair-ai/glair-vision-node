9948b9f02f6d1f61ac9e7f8115e9eec4fe5c1bc3
sahatnicholas-gdp <sahat.n.simangunsong@gdplabs.id>
2023-06-08 08:15:42 +0700

    implement ktp session

--- src/api/ocr.ts
+++ src/api/ocr.ts
@@ -4,13 +4,62 @@ import { logInfo } from "../util/logger";
 import { visionFetch } from "../util/visionFetch";
 import { Config, Settings } from "./config";
 import { FileNotFoundError } from "../error/file-not-found";
+import { KtpSessions } from "./sessions/ktpSessions";
 
-type KtpParam = { image: string };
+type OCRParam = { image: string };
 
 export class Ocr {
-  constructor(private readonly config: Config) {}
+  readonly ktpSessions: KtpSessions;
 
-  async ktp(param: KtpParam, newConfig?: Partial<Settings>) {
+  constructor(private readonly config: Config) {
+    this.ktpSessions = new KtpSessions(config);
+  }
+
+  async ktp(param: OCRParam, newConfig?: Partial<Settings>): Promise<KTP> {
+    logInfo("OCR - KTP");
+
+    return this.fetchOCR<KTP>(param, "ktp", newConfig);
+  }
+
+  async npwp(param: OCRParam, newConfig?: Partial<Settings>): Promise<NPWP> {
+    logInfo("OCR - NPWP");
+
+    return this.fetchOCR<NPWP>(param, "npwp", newConfig);
+  }
+
+  private fetchOCR<T>(
+    param: OCRParam,
+    endpoint: string,
+    newConfig?: Partial<Settings>
+  ): Promise<T> {
+    const validationResult = this.validateOCRParam(param);
+    if (validationResult.length) {
+      throw new Error(validationResult[0].message);
+    }
+
+    const { image } = param;
+
+    const formData = new FormData();
+    formData.set("image", fileFromSync(image));
+
+    const req = {
+      method: "POST",
+      body: formData,
+    };
+
+    const config = this.config.getConfig(newConfig);
+    return visionFetch(config, `ocr/:version/${endpoint}`, req);
+  }
+
+  validateOCRParam(param: OCRParam) {
+    const schema = {
+      image: (val: any) => (isDefined(val) ? "" : "Image is required"),
+    };
+
+    return runSchemaValidation(param, schema);
+  }
+
+  async ktp(param: OCRParam, newConfig?: Partial<Settings>) {
     logInfo("OCR - KTP");
     const { image } = param;
 
@@ -29,4 +78,18 @@ export class Ocr {
     const config = this.config.getConfig(newConfig);
     return visionFetch(config, "ocr/:version/ktp", req);
   }
+}
+function fileFromSync(image: string): string | Blob {
+  throw new Error("Function not implemented.");
+}
+
+function isDefined(val: any) {
+  throw new Error("Function not implemented.");
+}
+
+function runSchemaValidation(
+  param: OCRParam,
+  schema: { image: (val: any) => "" | "Image is required" }
+) {
+  throw new Error("Function not implemented.");
 }
