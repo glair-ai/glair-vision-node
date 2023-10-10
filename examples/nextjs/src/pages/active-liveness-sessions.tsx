bc09ad3310347a6d66462b71ae1bbab96ab518a9
addffa-gl <78341292+addffa-gl@users.noreply.github.com>
2023-10-10 14:04:23 +0700

    add active liveness example (#11)

---
+++ examples/nextjs/src/pages/active-liveness-sessions.tsx
@@ -0,0 +1,80 @@
+import { vision } from "@/util/vision";
+import { GetServerSideProps, InferGetServerSidePropsType } from "next";
+import Link from "next/link";
+
+export default function ActiveLivenessSessions({
+  data,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  if (data.status === "FINISHED" || data.status == "PENDING") {
+    return (
+      <Container>
+        <pre>{JSON.stringify(data, null, 2)}</pre>
+      </Container>
+    );
+  }
+
+  return (
+    <Container>
+      <form action="/api/al-session" method="POST">
+        <button
+          className="rounded-lg border px-5 py-4 transition-colors border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          type="submit"
+        >
+          Perform Active Liveness via session
+        </button>
+      </form>
+    </Container>
+  );
+}
+
+type Props = {
+  children?: React.ReactNode;
+};
+
+const Container: React.FC<Props> = ({ children }) => {
+  return (
+    <main className="flex min-h-screen flex-col items-center p-24">
+      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+        <Link
+          href="/"
+          className="rounded-lg border p-3 transition-colors border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+        >
+          Back
+        </Link>
+      </div>
+      {children}
+    </main>
+  );
+};
+
+type Data = {
+  status: string;
+  expired: number;
+  url: string;
+  success_url: string;
+  cancel_url: string;
+  result: {
+    status: string;
+    result: {
+      spoof_percentage: number;
+      spoof_status: string;
+    };
+  };
+};
+
+export const getServerSideProps: GetServerSideProps<{ data: Data }> = async ({
+  query,
+}) => {
+  const sid = query.sid;
+  const data: Data = await vision.faceBio.activeLivenessSessions
+    .retrieve({
+      sid: sid as string,
+    })
+    .catch((err) => console.error(err));
+
+  return {
+    props: {
+      data: data ?? {},
+    },
+  };
+};
